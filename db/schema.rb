97e39e1219976ee8dc3958eb6db1057d998f254a
Stevie <kamaustevek78@gmail.com>
2023-06-24 19:22:38 +0300

    Updated Schema

--- db/schema.rb
+++ db/schema.rb
@@ -12,16 +12,23 @@
 
 ActiveRecord::Schema[7.0].define(version: 2023_06_24_152959) do
   create_table "pizzas", force: :cascade do |t|
+    t.string "name"
+    t.string "ingredients"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
   create_table "restaurant_pizzas", force: :cascade do |t|
+    t.integer "price"
+    t.integer "pizza_id"
+    t.integer "restaurant_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
   create_table "restaurants", force: :cascade do |t|
+    t.string "name"
+    t.string "address"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
