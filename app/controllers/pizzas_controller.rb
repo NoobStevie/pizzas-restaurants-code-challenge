934c79d5ba0c26d5f505edf3ba042d4d6cb203cf
Stevie <kamaustevek78@gmail.com>
2023-06-24 22:25:54 +0300

    Added routes

--- app/controllers/pizzas_controller.rb
+++ app/controllers/pizzas_controller.rb
@@ -1,2 +1,22 @@
 class PizzasController < ApplicationController
+
+rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+rescue_from ActiveRecord::RecordInvalid, with: :unprocessable_entity_method
+
+    # GET all
+
+    def index
+        render json: Pizza.all, each_serializer: PizzaSerializer
+    end
+
+    def show
+        pizza = Pizza.find_by(id: params[:id])
+        render json: pizza
+    end
+
+    private
+
+    def record_not_found
+        render json:(error: "Pizzan not found"), status: :not_found
+    end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,54 +1,20 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+puts "start seeding..."
+PIZZAS = ["Neapolitan Pizza", "Chicago Pizza", "New York-Style Pizza", " Sicilian Pizza", "Greek Pizza"]
+INGREDIENTS = [ "tomatoes", "garlic", "oregano","extra virgin olive oil", "sausage", "pepperoni", "onion", "mushrooms","green peppers"]
+PRICE = [10,15,20,25,30]
 
-Restaurant.destroy_all
-Pizza.destroy_all
-RestaurantPizza.destroy_all
-
-# seeding restaurants
+# restaurants
 5.times do
-    Restaurant.create(
-        name: Faker::Restaurant.name,
-        address: Faker::Address.full_address
-    )
+    Restaurant.create(name: Faker::Restaurant.name, address: Faker::Address.street_address)
+    Pizza.create(name: PIZZAS.uniq.sample, ingredients: INGREDIENTS.uniq.sample(3))
 end
 
-# seeding pizzas
-Pizza.create([
-    {
-        name: "Pepperoni",
-        ingredients: "Dough, Tomato Sauce, Cheese, Pepperoni",
-        price: 10.99
+pizzas = Pizza.all
+restaurants = Restaurant.all
 
-    },
-    {
-        name: "Chicken Tikka",
-        ingredients: "Cheese, Chillies, Onion, Garlic, Chunks of chicken",
-        price: 8.49
-    },
-    {
-        name: "Barbeque Steak",
-        ingredients: "Steak, Dough, Barbeque, Tomato Sauce",
-        price: 7.23
-    },
-    {
-        name: "Veggie Pizza",
-        ingredients: "Tomato, Vegetables, Dough",
-        price: 6.99
-    }
-
-])
-
-# seeding join tables pizza-restaurant
-RestaurantPizza.create([
-    {restaurant_id: 1, pizza_id: 1, price: rand(1..30)},
-    {restaurant_id: 5, pizza_id: 2, price: rand(1..30)},
-    {restaurant_id: 2, pizza_id: 3, price: rand(1..30)},
-    {restaurant_id: 3, pizza_id: 4, price: rand(1..30)},
-    {restaurant_id: 4, pizza_id: 1, price: rand(1..30)}
-])
+pizza = pizzas.sample
+restaurant = restaurants.sample
+5.times do
+    RestaurantPizza.create(price: PRICE.sample, restaurant_id: restaurant.id,  pizza_id: pizza.id)
+end
+puts "done seeding"
